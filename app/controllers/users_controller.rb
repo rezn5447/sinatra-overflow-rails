7f9e7e9efce602fe826efd663786177513bf3785
Resdan Logan & Sam Leiken <pair+rezn5447.sleiken@devbootcamp.com>
2016-10-24 16:12:49 -0700

    successful bcrypt implementaion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   end
 
   def show
+
   end
 
   def new
@@ -12,10 +13,15 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    if @user.save
+      redirect_to users_show_url(@user)
+    else
+      render 'new'
+    end
   end
 
   private
   def user_params
-    params.require(:user).permit(:name, :password)
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,9 +17,9 @@ ActiveRecord::Schema.define(version: 20161024222314) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "password_hash"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
