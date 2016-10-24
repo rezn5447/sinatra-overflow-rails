773339391fcef493df6091a9ccdcf6f8d8743c6a
Resdan Logan & Sam Leiken <pair+rezn5447.sleiken@devbootcamp.com>
2016-10-24 15:32:49 -0700

    generate controller, model, routes, and views for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  get 'user/index'
+
+  get 'user/show'
+
+  get 'user/new'
+
+  get 'user/create'
+
+  resources :comments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
