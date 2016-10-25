a7d5c90788757c20e8594b4d3fc4e31c3571f1e1
Resdan Logan & Sam Leiken <pair+rezn5447.sleiken@devbootcamp.com>
2016-10-24 17:46:33 -0700

    create articles for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def show
-
+    @user = User.find(params[:id])
   end
 
   def new
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to users_url(@user)
+      redirect_to @user
     else
       render 'new'
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+
+  get 'articles/create'
+
+  get 'articles/destroy'
+
   root 'users#index'
   resources :sessions, only: [:new, :create, :destroy]
 
